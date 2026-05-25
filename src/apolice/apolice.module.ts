a3707f433faef3083000761fb98d13d10771d56b
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-05-25 18:49:57 -0300

    Fix: Apolices module

--- src/apolice/apolice.module.ts
+++ src/apolice/apolice.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Apolice } from './entities/apolice.entity';
 import { ApoliceService } from './service/apolice.service';
 import { ApoliceController } from './controller/apolice.controller';
 
+import { Usuario } from '../usuario/entities/usuario.entity';
+import { Plano } from '../plano/entities/plano.entity';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Apolice])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Apolice,
+      Usuario,
+      Plano,
+    ]),
+  ],
   providers: [ApoliceService],
   controllers: [ApoliceController],
-  exports: [ApoliceService]
+  exports: [ApoliceService],
 })
 export class ApoliceModule {}
